03368371ad1eeb53fbf45c8d37d5285fcd64539b
IordanAlexandru1997 <103210590+IordanAlexandru1997@users.noreply.github.com>
2023-02-28 06:17:23 +0100

    corrected max_capital

--- min/max heap/maximize_capital.py
+++ min/max heap/maximize_capital.py
@@ -1,21 +1,28 @@
 from heapq import *
 
 
-def maximum_capital(c, k, capitals, profits):
-    curr_cap = c
+def maximum_capital(k, w, capital, profits):
+    curr_cap = w
     capital_minH = []
     profit_maxH = []
 
-    for x in range(len(capitals)):
-        heappush(capital_minH, (capitals[x], x))
+    for x in range(len(capital)):
+        heappush(capital_minH, (capital[x], x))
+
     for _ in range(k):
+
         while capital_minH and capital_minH[0][0] <= curr_cap:
-            c, i = heappop(capital_minH)
-            heappush(profit_maxH, (-profits[i], i))
+            heappush(profit_maxH, -profits[capital_minH[0][1]])
+            heappop(capital_minH)
         if not profit_maxH:
             break
+        curr_cap += -(heappop(profit_maxH))
 
-        j = -heappop(profit_maxH)[0]
-        # print(f"\t\tUpdated capital = {current_capital} + {j}")
-        curr_cap += j
     return curr_cap
+
+
+k = 2
+w = 0
+profits = [1, 2, 3]
+capital = [0, 1, 1]
+print(maximum_capital(k, w, capital, profits))
